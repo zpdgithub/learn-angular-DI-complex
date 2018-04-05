326e4c70c5c24e8feb55529be91dab699e35fc96
peter <peter@soez.io>
2018-04-05 19:30:28 +0800

    在部件的构造函数中声明可注入对象

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Component } from '@angular/core';
 
+import { ApiService } from './services/ApiService';
+
 @Component({
   selector: 'app-root',
   template: `root`,
 })
 export class AppComponent {
+  constructor(
+    private apiService: ApiService  // 在部件的构造函数中声明可注入对象
+  ) {
+  }
 }
 
 @NgModule({
@@ -16,7 +22,9 @@ export class AppComponent {
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    ApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
